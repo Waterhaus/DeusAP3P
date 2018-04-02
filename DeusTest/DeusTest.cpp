d7780dd9d696b8284e73dad531fbdb8b71cfa0fc
Denis Kokorev <nvsbleat@gmail.com>
2018-04-02 15:27:23 +0300

    Добавьте файлы проекта.

---
+++ DeusTest/DeusTest.cpp
@@ -0,0 +1,14 @@
+// DeusTest.cpp: определяет точку входа для консольного приложения.
+//
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    return 0;
+}
+
